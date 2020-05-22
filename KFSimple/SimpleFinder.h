cca98b4056ef02bdbb235be05d82bbcd4d84fb8e
Oleksii Lubynets <lubynets@lxbk0200.gsi.de>
2020-05-22 23:29:29 +0200

    chi2prim vars remain only

--- KFSimple/SimpleFinder.h
+++ KFSimple/SimpleFinder.h
@@ -20,7 +20,7 @@
 #include "KFParticleSIMD.h"
 #include "Constants.h"
 #include "InputContainer.h"
-#include "CutsContainer.h"
+// #include "CutsContainer.h"
 #include "OutputContainer.h"
 
 class SimpleFinder
@@ -37,42 +37,45 @@ class SimpleFinder
   void  SortTracks();
   void  FindParticles();
   
-  const KFPTrackVector* GetTracks() const {return &tracks_;};
+//   const KFPTrackVector* GetTracks() const {return &tracks_;};
+//   
+//   const std::vector<float>& GetMass() const {return vec_mass_;};            // TODO remove after debug procedure
+//   
+//   const std::vector<OutputContainer>& GetLambdas() const {return vec_lambda_;};
+  const std::vector<OutputContainer>& GetVecTracks() const { return vec_tracks_; };
   
-  const std::vector<float>& GetMass() const {return vec_mass_;};            // TODO remove after debug procedure
+//   
+//   void  SetCuts(const CutsContainer& cuts) { cuts_ = cuts; }
   
-  const std::vector<OutputContainer>& GetLambdas() const {return vec_lambda_;};
-  
-  void  SetCuts(const CutsContainer& cuts) { cuts_ = cuts; }
-
  protected:
-   
+  
   float CalculateChiToPrimaryVertex(const KFPTrack &track, int pid) const;  ///< Calculates \f$\chi^2\f$ of the track to the primary vertex (PV)
-  void  CalculateParamsInPCA(const KFPTrack &track1, int pid1, const KFPTrack &track2, int pid2, std::array<float, 8> &pars1, std::array<float, 8> &pars2) const;
-  ///< Recalculates daughters tracks' parameters in the point of their closest approach
-  
-  float CalculateDistanceBetweenParticles(const std::array<float, 8> &pars1, const std::array<float, 8> &pars2) const;  ///< Calculates the distance between daughter tracks in their closest approach
-  float CalculateCosMomentumSum(const std::array<float, 8> &pars1, const std::array<float, 8> &pars2) const;    ///< Calculates the cosine of the angle between daughter's and mother's momenta
-  KFParticleSIMD ConstructMother(const KFPTrack &track1, int pid1, const KFPTrack &track2, int pid2) const;   ///< Creates mother particle as the KFParticleSIMD object
-  float CalculateChi2Geo(const KFParticleSIMD& mother) const;  ///< Calculates \f$\chi^2\f$ of daughters' tracks in their closest approach
-  void  CalculateMotherProperties(const KFParticleSIMD& mother, float &l, float &ldl, int &isFromPV) const;
-  ///< Calculates distance between primary and secondary vertices with error and determines whether mother comes from the PV
-  
-  float CalculateCosTopo(const KFParticleSIMD& mother) const;  ///< Calculates cosine of the angle between reconstructed mother's momentum and mother's radius vector beginning in the PV
-  float CalculateChi2Topo(const KFParticleSIMD& mother) const; ///< Calculates \f$\chi^2\f$ of the mother's track to the PV
+//   void  CalculateParamsInPCA(const KFPTrack &track1, int pid1, const KFPTrack &track2, int pid2, std::array<float, 8> &pars1, std::array<float, 8> &pars2) const;
+//   ///< Recalculates daughters tracks' parameters in the point of their closest approach
+//   
+//   float CalculateDistanceBetweenParticles(const std::array<float, 8> &pars1, const std::array<float, 8> &pars2) const;  ///< Calculates the distance between daughter tracks in their closest approach
+//   float CalculateCosMomentumSum(const std::array<float, 8> &pars1, const std::array<float, 8> &pars2) const;    ///< Calculates the cosine of the angle between daughter's and mother's momenta
+//   KFParticleSIMD ConstructMother(const KFPTrack &track1, int pid1, const KFPTrack &track2, int pid2) const;   ///< Creates mother particle as the KFParticleSIMD object
+//   float CalculateChi2Geo(const KFParticleSIMD& mother) const;  ///< Calculates \f$\chi^2\f$ of daughters' tracks in their closest approach
+//   void  CalculateMotherProperties(const KFParticleSIMD& mother, float &l, float &ldl, int &isFromPV) const;
+//   ///< Calculates distance between primary and secondary vertices with error and determines whether mother comes from the PV
+//   
+//   float CalculateCosTopo(const KFParticleSIMD& mother) const;  ///< Calculates cosine of the angle between reconstructed mother's momentum and mother's radius vector beginning in the PV
+//   float CalculateChi2Topo(const KFParticleSIMD& mother) const; ///< Calculates \f$\chi^2\f$ of the mother's track to the PV
   void  SaveParticle(const OutputContainer& Lambda);                 ///< Saves selected particle with set of geometrical decay parameters
-  
+//   
   KFPTrackVector tracks_;
   KFVertex prim_vx_;
   
   std::array<std::vector<int>, kNumberOfTrackTypes> trIndex_;
-             
-  CutsContainer cuts_;
-  
-  float mass_{0.};                             // TODO remove after debug procedure
-  std::vector<float> vec_mass_{};            // TODO remove after debug procedure
+//              
+//   CutsContainer cuts_;
+//   
+//   float mass_{0.};                             // TODO remove after debug procedure
+//   std::vector<float> vec_mass_{};            // TODO remove after debug procedure
+// 
+  std::vector<OutputContainer> vec_tracks_{};
 
-  std::vector<OutputContainer> vec_lambda_{};
 };
 
 #endif//SimpleFinder_H
